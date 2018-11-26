4e47703f13b0a50fedb109a195e15dcdb686e889
Rui Guedes <rui@MacBook-Air-de-Rui.local>
2018-11-26 14:01:01 +0000

    all charts

--- alix-app/src/app/charts/bar/bar.component.ts
+++ alix-app/src/app/charts/bar/bar.component.ts
@@ -1,32 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-area',
-    templateUrl: './area.component.html',
-    styleUrls: ['./area.component.scss']
+    selector: 'alix-bar',
+    templateUrl: './bar.component.html',
+    styleUrls: ['./bar.component.scss']
 })
-export class AreaComponent implements OnInit {
-    public options = {};
+export class BarComponent implements OnInit {
+    
+    public options: any = {};
 
-    constructor() { }
+    ngOnInit() { 
 
-    ngOnInit() {
-
-        const xAxisData = [];
-        const data1 = [];
-        const data2 = [];
-
-        for (let i = 0; i < 100; i++) {
-            xAxisData.push('category' + i);
+        var xAxisData = [];
+        var data1 = [];
+        var data2 = [];
+        for (var i = 0; i < 100; i++) {
+            xAxisData.push('类目' + i);
             data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
             data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
         }
 
-        this.options = {
-            
+        this.options  = {
+            title: {
+                text: '柱状图动画延迟'
+            },
             legend: {
                 data: ['bar', 'bar2'],
                 align: 'left'
+            },
+            toolbox: {
+                // y: 'bottom',
+                feature: {
+                    magicType: {
+                        type: ['stack', 'tiled']
+                    },
+                    dataView: {},
+                    saveAsImage: {
+                        pixelRatio: 2
+                    }
+                }
             },
             tooltip: {},
             xAxis: {
@@ -58,6 +70,9 @@ export class AreaComponent implements OnInit {
                 return idx * 5;
             }
         };
+    }
+
+    constructor() {
     }
 
 }
--- alix-app/src/app/dashboard/dashboard/dashboard.component.ts
+++ alix-app/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { MockapiService } from "../../_services/_mockapi.service";
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss']
+    styleUrls: ['./dashboard.component.scss'],
+    providers:[MockapiService]
 })
 export class DashboardComponent implements OnInit {
 
 
 
-    constructor() { }
+    constructor( private api: MockapiService) { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+
+        this.api.getJSON().subscribe(data => { 
+            
+            console.log(data);
+        });
+    }
 }
