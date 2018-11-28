e0a7181364748f53ee7203b50778d46602e2325a
Joseph Briggs <jbriggsr3@gmail.com>
2018-11-28 21:52:54 +0000

    WIP - #56 - Financial Panel-1

--- alix-app/src/app/pages/portfolio/financial/financial.component.ts
+++ alix-app/src/app/pages/portfolio/financial/financial.component.ts
@@ -11,17 +11,13 @@ import * as dataset from '../../../data/dataset';
 })
 export class FinancialComponent implements OnInit {
     public config: any;
-    private dossierId: any;
 
     constructor(private route: ActivatedRoute) {}
 
     ngOnInit() {
-      // console.log(dataset.dossiersMainData);
-      // console.log(this.route.snapshot.params['id']);
-
       this.route.params.subscribe( params => {
-        this.dossierId = params['id'];
-        console.log(dataset.dossiersMainData['dossier' + this.dossierId]);
+        this.config = dataset.dossiersMainData['dossier' + params['id']];
+        console.log(this.config)
       });
     }
 }
