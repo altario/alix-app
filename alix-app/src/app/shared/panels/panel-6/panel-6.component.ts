9ff74f82b0d7ed85cfed4fa5d08c3deb7c49e8ec
Joseph Briggs <jbriggsr3@gmail.com>
2018-11-29 10:42:49 +0000

    WIP - #56 - added panel 8 &&
    * added correct values on panel-1

--- alix-app/src/app/shared/panels/panel-6/panel-6.component.ts
+++ alix-app/src/app/shared/panels/panel-6/panel-6.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-    selector: "app-panel-6",
-    templateUrl: "./panel-6.component.html",
-    styleUrls: ["./panel-6.component.scss"]
+    selector: 'app-panel-6',
+    templateUrl: './panel-6.component.html',
+    styleUrls: ['./panel-6.component.scss']
 })
 export class Panel6Component implements OnInit {
     @Input()
