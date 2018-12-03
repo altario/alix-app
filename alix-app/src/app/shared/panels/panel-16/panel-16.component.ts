4fefa61007c42bebc7373e727a9cc506013e231a
Joseph Briggs <jbriggsr3@gmail.com>
2018-12-03 13:17:35 +0000

    Added Panel 16 / 17; fixed panel-9

---
+++ alix-app/src/app/shared/panels/panel-16/panel-16.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-16',
+  templateUrl: './panel-16.component.html',
+  styleUrls: ['./panel-16.component.scss']
+})
+export class Panel16Component implements OnInit {
+
+  @Input()
+  public config: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ alix-app/src/app/shared/panels/panel-17/panel-17.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-17',
+  templateUrl: './panel-17.component.html',
+  styleUrls: ['./panel-17.component.scss']
+})
+export class Panel17Component implements OnInit {
+
+  @Input()
+  public config: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
