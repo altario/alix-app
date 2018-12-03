990cc50b8d2d02109a14778a1f71f9f0d28a237d
Rui Guedes <rui@Air-de-Rui.lan>
2018-12-03 14:12:19 +0000

    chart area basic interaction with dropdown

--- alix-app/src/app/pages/portfolio/demand-offer/demand-offer.component.ts
+++ alix-app/src/app/pages/portfolio/demand-offer/demand-offer.component.ts
@@ -7,111 +7,170 @@ import * as chartdataset from '@data/charts-dataset';
 
 
 @Component({
-    selector: 'app-demand-offer',
-    templateUrl: './demand-offer.component.html',
-    styleUrls: ['./demand-offer.component.scss']
+  selector: 'app-demand-offer',
+  templateUrl: './demand-offer.component.html',
+  styleUrls: ['./demand-offer.component.scss']
 })
 export class DemandAndOfferComponent implements OnInit {
-    public config: object;
-    public series: any;
-    public opts: any;
-    constructor(private route: ActivatedRoute) {}
+  public config: object;
+  public series: any;
+  public opts: any;
+  public chartInstance: any = {};
 
-    ngOnInit() {
-        this.route.params.subscribe(params => {
-            // console.log(params['id']);
-            this.config = dataset.dossiersMainData.dossier1.demandAndOffer;
-            console.log(this.config)
+  constructor(private route: ActivatedRoute) { }
 
-            this.opts = {
-                marketValue: {
-                  legend: {
-                    data: [
-                      chartdataset.dossier1ChartsData.demandOffer.mktValueVsReplacementCost.marketValueSqm.label,
-                      chartdataset.dossier1ChartsData.demandOffer.mktValueVsReplacementCost.replacementCostSqm.label
-                    ]
-                  },
-                  tooltip: {
-                    trigger: 'axis',
-                    axisPointer: {
-                      type: 'cross',
-                      label: {
-                        backgroundColor: '#6a7985'
-                      }
-                    }
-                  },
-                  xAxis: {
-                      type: 'category',
-                      data: chartdataset.dossier1ChartsData.demandOffer.mktValueVsReplacementCost.year.values
-                  }
-                },
-                sqm: {
-                    title: {
-                        text: '# SQM / Asset Demand'
-                    },
-                },
-                rooms: {
-                    title: {
-                        text: '# Rooms / Asset Demand'
-                    },
-                }
-            };
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      // console.log(params['id']);
+      this.config = dataset.dossiersMainData.dossier1.demandAndOffer;
 
 
+      this.opts = {
+        marketValue: {
+          legend: {
+            data: [
+              chartdataset.dossier1ChartsData.demandOffer.neighborhoodMktValueVsReplacementCost.marketValueSqm.label,
+              chartdataset.dossier1ChartsData.demandOffer.neighborhoodMktValueVsReplacementCost.replacementCostSqm.label
+            ]
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#6a7985'
+              }
+            }
+          },
+          xAxis: {
+            type: 'category',
+            data: chartdataset.dossier1ChartsData.demandOffer.neighborhoodMktValueVsReplacementCost.year.values
+          }
+        },
+        sqm: {
+          title: {
+            text: '# SQM / Asset Demand'
+          },
+        },
+        rooms: {
+          title: {
+            text: '# Rooms / Asset Demand'
+          },
+        }
+      };
 
-            this.series = {
-                marketValue: [{
-                  name: chartdataset.dossier1ChartsData.demandOffer.mktValueVsReplacementCost.marketValueSqm.label,
-                  data: chartdataset.dossier1ChartsData.demandOffer.mktValueVsReplacementCost.marketValueSqm.values,
-                  type: 'line'
-                }, {
-                  name: chartdataset.dossier1ChartsData.demandOffer.mktValueVsReplacementCost.replacementCostSqm.label,
-                  data: chartdataset.dossier1ChartsData.demandOffer.mktValueVsReplacementCost.replacementCostSqm.values,
-                  type: 'line'
-                }],
 
 
-                sqm: [{
-                    symbolSize: 20,
-                    data: [
-                        [10.0, 8.04],
-                        [8.0, 6.95],
-                        [13.0, 7.58],
-                        [9.0, 8.81],
-                        [11.0, 8.33],
-                        [14.0, 9.96],
-                        [6.0, 7.24],
-                        [4.0, 4.26],
-                        [12.0, 10.84],
-                        [7.0, 4.82],
-                        [5.0, 5.68]
-                    ],
-                    type: 'scatter'
-                }],
-                rooms: [{
-                    symbolSize: 20,
-                    data: [
-                        [6.0, 7.24],
-                        [4.0, 4.26],
-                        [12.0, 10.84],
-                        [7.0, 4.82],
-                        [5.0, 5.68],
-                        [10.0, 8.04],
-                        [8.0, 6.95],
-                        [6.0, 7.24],
-                        [4.0, 4.26],
-                        [12.0, 10.84],
-                        [7.0, 4.82],
-                        [5.0, 5.68],
-                        [13.0, 7.58],
-                        [9.0, 8.81],
-                        [11.0, 8.33],
-                        [14.0, 9.96]
-                    ],
-                    type: 'scatter'
-                }]
+      this.series = {
+        marketValue: [{
+          name: chartdataset.dossier1ChartsData.demandOffer.neighborhoodMktValueVsReplacementCost.marketValueSqm.label,
+          data: chartdataset.dossier1ChartsData.demandOffer.neighborhoodMktValueVsReplacementCost.marketValueSqm.values,
+          type: 'line'
+        }, {
+          name: chartdataset.dossier1ChartsData.demandOffer.neighborhoodMktValueVsReplacementCost.replacementCostSqm.label,
+          data: chartdataset.dossier1ChartsData.demandOffer.neighborhoodMktValueVsReplacementCost.replacementCostSqm.values,
+          type: 'line'
+        }],
 
-            };
-        });
-    }
+
+        sqm: [{
+          symbolSize: 20,
+          data: [
+            [10.0, 8.04],
+            [8.0, 6.95],
+            [13.0, 7.58],
+            [9.0, 8.81],
+            [11.0, 8.33],
+            [14.0, 9.96],
+            [6.0, 7.24],
+            [4.0, 4.26],
+            [12.0, 10.84],
+            [7.0, 4.82],
+            [5.0, 5.68]
+          ],
+          type: 'scatter'
+        }],
+        rooms: [{
+          symbolSize: 20,
+          data: [
+            [6.0, 7.24],
+            [4.0, 4.26],
+            [12.0, 10.84],
+            [7.0, 4.82],
+            [5.0, 5.68],
+            [10.0, 8.04],
+            [8.0, 6.95],
+            [6.0, 7.24],
+            [4.0, 4.26],
+            [12.0, 10.84],
+            [7.0, 4.82],
+            [5.0, 5.68],
+            [13.0, 7.58],
+            [9.0, 8.81],
+            [11.0, 8.33],
+            [14.0, 9.96]
+          ],
+          type: 'scatter'
+        }],
+      };
+    });
+
+    this.priceTodayVsOvertime();
+  }
+
+  priceTodayVsOvertime() {
+
+    this.opts.priceTodayVsOvertime = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        }
+      },
+      xAxis: {
+        type: 'category',
+        data: chartdataset.dossier1ChartsData.demandOffer.priceTodayVsOvertime.year.values
+      }
+    };
+
+    this.series.priceTodayVsOvertime = [{
+      name: chartdataset.dossier1ChartsData.demandOffer.priceTodayVsOvertime['assetPrice'].label,
+      data: chartdataset.dossier1ChartsData.demandOffer.priceTodayVsOvertime['assetPrice'].values,
+      type: 'line',
+      areaStyle: {}
+    }];
+
+    console.log(this.series.priceTodayVsOvertime)
+  }
+
+  getPriceTodayVsOvertime(): Array<any> {
+    const options = Object.keys(chartdataset.dossier1ChartsData.demandOffer.priceTodayVsOvertime).map((population, i) => {
+        return { id: i-1, key: population, value: chartdataset.dossier1ChartsData.demandOffer.priceTodayVsOvertime[population].label };
+    });
+
+    options.shift();
+    return options;
+  }
+  changePriceTodayVsOvertime(callbackEvent, chartInstance ): void {
+    this.series.priceTodayVsOvertime = [{
+      name: chartdataset.dossier1ChartsData.demandOffer.priceTodayVsOvertime[callbackEvent].label,
+      data: chartdataset.dossier1ChartsData.demandOffer.priceTodayVsOvertime[callbackEvent].values,
+      type: 'line',
+      areaStyle: {}
+    }];
+
+    this.chartInstance.priceTodayVsOvertime.setOption({
+      series: this.series.priceTodayVsOvertime
+    });
+
+    console.log(this.series.priceTodayVsOvertime);
+  }
+
+  onChartInit(chart, e) {
+    this.chartInstance[chart] = e;
+    console.log(this.chartInstance);
+  }
 }
