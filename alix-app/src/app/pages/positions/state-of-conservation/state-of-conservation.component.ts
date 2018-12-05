efa3f04818f72626b11cbe1ca9165d3869801409
Joseph Briggs <jbriggsr3@gmail.com>
2018-12-05 22:50:13 +0000

    Styled Graph Price evolution by state of conservation

--- alix-app/src/app/pages/positions/state-of-conservation/state-of-conservation.component.ts
+++ alix-app/src/app/pages/positions/state-of-conservation/state-of-conservation.component.ts
@@ -144,26 +144,52 @@ export class StateOfConservationComponent implements OnInit {
   }
 
   getpriceEvolutionBySc(population = 'population1'): Array<any> {
-    const popSelected = chartdataset.dossier1ChartsData.stateOfConservation.priceEvolutionBySc.filter((line) => {
-      if (line.population === population) {
-        return line;
-      }
-    });
+    const lineColors = ['#00B5E9', '#7AC143', '#C7DA2C', '#F2E603', '#FCB86B', '#E9545C'];
+    const popSelected = chartdataset.dossier1ChartsData.stateOfConservation.priceEvolutionBySc.filter(line => line.population === population);
 
     this.opts.priceEvolutionBySc = {
-      legend: {
-        data: popSelected.map(line => line.stateOfConservation)
+      title: {
+        text: 'PRICE EVOLUTION BY STATE OF CONSERVATION', // #HC
+        top: '17px',
+        left: '16px',
+        textStyle: eChartsConfig.title,
       },
+      legend: {
+        data: popSelected.map(line => ({name: line.stateOfConservation, icon: 'rect'})),
+        itemWidth: eChartsConfig.legend.itemWidth,
+        itemHeight: eChartsConfig.legend.itemHeight,
+        top: '55px',
+        right: eChartsConfig.legend.right,
+        textStyle: {
+          fontSize: eChartsConfig.legend.fontSize,
+          color: eChartsConfig.legend.color
+        }
+      },
+      grid: eChartsConfig.grid,
       xAxis: {
         type: 'category',
-        data: popSelected.map(line => line.stateOfConservation)
+        data: popSelected.map(line => line.stateOfConservation),
+        splitLine: eChartsConfig.xAxis.splitLine,
+        axisLabel: eChartsConfig.xAxis.axisLabel,
+        axisLine: eChartsConfig.xAxis.axisLine
       },
       yAxis: {
-        type: 'value'
-      },
+        type: 'value',
+        splitLine: eChartsConfig.yAxis.splitLine,
+        axisLabel: eChartsConfig.yAxis.axisLabel,
+        axisLine: eChartsConfig.yAxis.axisLine
+      }
     };
 
-    const series = popSelected.map((line: any) => ({ name: line.stateOfConservation, data: line.values, type: 'line' }));
+    const series = popSelected.map((line: any, i) => ({
+      name: line.stateOfConservation,
+      data: line.values,
+      type: 'line',
+      symbol: eChartsConfig.series.symbol,
+      symbolSize: eChartsConfig.series.symbolSize,
+      lineStyle: {...eChartsConfig.series.lineStyle, color: lineColors[i] },
+      itemStyle: { color: lineColors[i] }
+    }));
 
     console.log(series);
     return series;
