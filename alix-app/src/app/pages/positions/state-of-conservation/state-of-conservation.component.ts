eb9e2e0803f933844c675bc4745f25fb3cd9ab41
Joseph Briggs <jbriggsr3@gmail.com>
2018-12-06 18:45:55 +0000

    Changed x-axis information on line graphs; removed legend from doughnout graph

--- alix-app/src/app/pages/positions/state-of-conservation/state-of-conservation.component.ts
+++ alix-app/src/app/pages/positions/state-of-conservation/state-of-conservation.component.ts
@@ -91,7 +91,7 @@ export class StateOfConservationComponent implements OnInit {
 
     this.opts.numbOfAssetsBySc = {
       title: {
-        text: '# By State of conservation', // #HC
+        text: '# BBY STATE OF CONSERVATION', // #HC
         top: '17px',
         left: '16px',
         textStyle: eChartsConfig.title,
@@ -110,7 +110,7 @@ export class StateOfConservationComponent implements OnInit {
       grid: eChartsConfig.grid,
       xAxis: {
         type: 'category',
-        data: popSelected.map(line => line.stateOfConservation),
+        data: [2013, 2014, 2015, 2016, 2017, 2018],
         splitLine: eChartsConfig.xAxis.splitLine,
         axisLabel: eChartsConfig.xAxis.axisLabel,
         axisLine: eChartsConfig.xAxis.axisLine
@@ -170,7 +170,7 @@ export class StateOfConservationComponent implements OnInit {
       grid: eChartsConfig.grid,
       xAxis: {
         type: 'category',
-        data: popSelected.map(line => line.stateOfConservation),
+        data: [2013, 2014, 2015, 2016, 2017, 2018],
         splitLine: eChartsConfig.xAxis.splitLine,
         axisLabel: eChartsConfig.xAxis.axisLabel,
         axisLine: eChartsConfig.xAxis.axisLine
