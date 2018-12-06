63cc45062937a07e0f9b8eec8166fcd6795c2c0d
Joseph Briggs <jbriggsr3@gmail.com>
2018-12-06 19:48:06 +0000

    Changed pipe to allow icon positive or negative

--- alix-app/src/app/helpers/percentage/percentage.pipe.ts
+++ alix-app/src/app/helpers/percentage/percentage.pipe.ts
@@ -3,7 +3,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'perc' })
 
 export class CustomPercentagePipe implements PipeTransform {
-  transform(value: number, decimal: number = 2) {
-    return isNaN(value) ? value : (value * 100).toFixed(decimal);
+  transform(value: any, decimal: number = 2, icon = null) {
+    const val = isNaN(value) ? value : (value * 100).toFixed(decimal);
+
+    if (icon === 'icon') {
+      return !isNaN(val) ? `<span class="${ Math.sign(val) === 1 ?
+        'text--success postive' : 'text--error negative' }">${ val }%</span>` : `<span>${ val }</span>`;
+    }
+    return val;
   }
 }
