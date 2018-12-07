abaae5101e3ec6fa792831b46293cfcb869e9a9b
Rui Guedes <rui@MacBook-Air-de-Rui.local>
2018-12-07 23:43:27 +0000

    fix no dashboard

--- alix-app/src/app/pages/positions/location/location.component.ts
+++ alix-app/src/app/pages/positions/location/location.component.ts
@@ -8,6 +8,7 @@ import * as chartdataset from '@data/charts-dataset';
 
 // graph color overrides
 import { eChartsConfig } from '@global/charts';
+import * as plotchartdataset from '@data/plotcharts-dataset';
 
 @Component({
   selector: 'app-location',
@@ -23,7 +24,8 @@ export class LocationComponent implements OnInit {
   lng: number = 9.1911364;
   zoom = 14;
   radius = 70;
-  
+  opts: any = {};
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -32,7 +34,6 @@ export class LocationComponent implements OnInit {
       this.config = dataset.dossiersMainData.dossier1.location;
       this.vehicleValues = this.initVehiclesValues();
       this.selectedVehicleYear = this.config.vehicles.years[this.vehicleValues[0]];
-      console.log(this.config);
     });
   }
 
@@ -75,18 +76,18 @@ export class LocationComponent implements OnInit {
   getbreakdownAssetsInNeighborhoodSeries(): Array<any> {
 
     return Object.keys(chartdataset.dossier1ChartsData.location.breakdownAssetsInNeighborhood).map((population, i) => {
-      if ( population !== 'year' ) {
+      if (population !== 'year') {
         return {
           name: population,
-            type: 'bar',
-            stack: 'one',
-            label: {
-                normal: {
-                    show: false,
-                    position: 'insideRight'
-                }
-            },
-            data: chartdataset.dossier1ChartsData.location.breakdownAssetsInNeighborhood[population].values
+          type: 'bar',
+          stack: 'one',
+          label: {
+            normal: {
+              show: false,
+              position: 'insideRight'
+            }
+          },
+          data: chartdataset.dossier1ChartsData.location.breakdownAssetsInNeighborhood[population].values
         };
       }
     });
@@ -98,9 +99,9 @@ export class LocationComponent implements OnInit {
       color: ['#003366', '#006699', '#4cabce', '#e5323e'],
       calculable: true,
       legend: {
-          show:false,
+        show: false,
         data: Object.keys(chartdataset.dossier1ChartsData.location.percentageAccommByPricePerRoom).map((population, i) => {
-            return population;
+          return population;
         }),
         align: 'left'
       },
@@ -111,13 +112,13 @@ export class LocationComponent implements OnInit {
         axisLine: eChartsConfig.xAxis.axisLine,
         axisTick: { show: false },
         data: chartdataset.dossier1ChartsData.location.percentageAccommByPricePerRoom.pricePerNight.values.map((population, i) => {
-          return '>' +population + '€';
+          return '>' + population + '€';
         })
       },
       yAxis: {
         type: 'value',
-          splitLine: eChartsConfig.yAxis.splitLine,
-          axisLabel: eChartsConfig.yAxis.axisLabel
+        splitLine: eChartsConfig.yAxis.splitLine,
+        axisLabel: eChartsConfig.yAxis.axisLabel
       }
     };
   }
@@ -150,16 +151,65 @@ export class LocationComponent implements OnInit {
     };
 
     return Object.keys(chartdataset.dossier1ChartsData.location.percentageAccommByPricePerRoom).map((population, i) => {
-        if (population != 'pricePerNight') {
-            return {
-            name: population,
-            barGap: 0,
-            type: 'bar',
-            label: labelOption,
-            data: chartdataset.dossier1ChartsData.location.percentageAccommByPricePerRoom[population].values
-            };
-        }
+      if (population != 'pricePerNight') {
+        return {
+          name: population,
+          barGap: 0,
+          type: 'bar',
+          label: labelOption,
+          data: chartdataset.dossier1ChartsData.location.percentageAccommByPricePerRoom[population].values
+        };
+      }
 
     });
+  }
+
+  getscpVehiclesRad() {
+    this.opts.scpVehiclesRad = {
+      grid: {
+        left: 100,
+        right: eChartsConfig.grid.right
+      },
+      legend: {
+        data: [
+          { name: 'Commercial', icon: 'rect' },
+          { name: 'Standard', icon: 'rect' },
+          { name: 'Family', icon: 'rect' },
+          { name: 'Mini', icon: 'rect' },
+          { name: 'Motorcycle', icon: 'rect' },
+          { name: 'Luxury', icon: 'rect' },
+          { name: 'Sport', icon: 'rect' }
+        ],
+        itemWidth: eChartsConfig.legend.itemWidth,
+        itemHeight: eChartsConfig.legend.itemHeight,
+        top: eChartsConfig.legend.top,
+        right: eChartsConfig.legend.right,
+        textStyle: {
+          fontSize: eChartsConfig.legend.fontSize,
+          color: eChartsConfig.legend.color
+        }
+
+      },
+      xAxis: {
+        type: 'value',
+        splitLine: eChartsConfig.xAxis.splitLine,
+        axisLabel: eChartsConfig.xAxis.axisLabel,
+        axisLine: eChartsConfig.xAxis.axisLine
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: eChartsConfig.yAxis.splitLine,
+        axisLabel: eChartsConfig.yAxis.axisLabel,
+        axisLine: eChartsConfig.yAxis.axisLine
+      }
+    };
+
+    const series = plotchartdataset.dossier1PlotChartsData.stateOfConservation.scpPriceEvo.filter((line) => {
+      if (line.population === 'population1') {
+        return line;
+      }
+    });
+
+    return series.shift().values;
   }
 }
