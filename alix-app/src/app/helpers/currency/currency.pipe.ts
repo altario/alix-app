60606e0d36fb9fcebeea6b03cc55ac275f9e0400
Joseph Briggs <jbriggsr3@gmail.com>
2018-12-09 13:12:14 +0000

    Added chart to custom portfolio; started with pipe condition

--- alix-app/src/app/helpers/currency/currency.pipe.ts
+++ alix-app/src/app/helpers/currency/currency.pipe.ts
@@ -4,16 +4,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class CustomCurrencyPipe implements PipeTransform {
   transform(value: number, decimal: number = 2) {
-    switch (true) {
-      case value < 1000000 && value >= 1000:
-        return (value / 1000).toFixed(decimal);
-      case value < 100000000 && value >= 1000000:
-        return (value / 1000000).toFixed(decimal);
-      case value >= 100000000:
-        return (value / 100000000).toFixed(decimal);
+    // switch (true) {
+    //   case value < 1000000 && value >= 1000:
+    //     return (value / 1000).toFixed(decimal);
+    //   case value < 100000000 && value >= 1000000:
+    //     return (value / 1000000).toFixed(decimal);
+    //   case value >= 100000000:
+    //     return (value / 100000000).toFixed(decimal);
 
-      default:
-        return (value / 1).toFixed(2);
-    }
+    //   default:
+    //     return (value / 1).toFixed(2);
+    // }
+
+    if (value === null) { return null; } // terminate early
+    if (value === 0) { return '0'; } // terminate early
+
+    const b = Math.abs(value).toPrecision(2).split('e'); // get power
+    // console.log(typeof b)
+    const k = b.length === 1 ? 0 : Math.floor(Math.min(parseInt(b[1].slice(1), 10), 14) / 3); // floor at decimals, ceiling at trillions
+    const c = k < 1 ? value.toFixed(0 + decimal) : (value / Math.pow(10, k * 3) ).toFixed(1 + decimal); // divide by power
+    const d = parseInt(c, 10) < 0 ? c : Math.abs(parseInt(c, 10)); // enforce -0 is 0
+    const e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+
+    return e;
   }
 }
--- alix-app/src/app/pages/portfolio/portfolio-detail/portfolio-detail.component.ts
+++ alix-app/src/app/pages/portfolio/portfolio-detail/portfolio-detail.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 import { MockapiService } from '../../../services/mockapi.service';
 import { Observable } from 'rxjs';
 import { map, tap, mergeMap, filter } from 'rxjs/operators';
+import { eChartsConfig } from '@app/global/charts';
 
 @Component({
   selector: 'app-portfolio-detail',
@@ -15,6 +16,7 @@ export class PortfolioDetailComponent implements OnInit {
   public portfolio$: Observable<any>;
   public type$: Observable<any>;
   public notification$: Observable<any>;
+  public chart: any;
 
   constructor(private apiService: MockapiService, private route: ActivatedRoute) {}
 
@@ -30,5 +32,72 @@ export class PortfolioDetailComponent implements OnInit {
       filter(params => !!params.id),
       mergeMap(params => this.apiService.getNotificationPopulated(params.id))
     );
+
+    this.chart = {
+      opts: {},
+      series: {}
+    };
+
+    this.setupChart();
+
+  }
+
+  setupChart() {
+    this.notification$.subscribe(res => {
+      const lineColors = ['#FFFFFF', '#00B5E9', '#7AC143'];
+      this.chart.opts = {
+        title: {
+          text: res.chartName,
+          top: '17px',
+          left: '125',
+          textStyle: eChartsConfig.title,
+        },
+        legend: {
+          data: [res.chartName],
+          itemWidth: eChartsConfig.legend.itemWidth,
+          itemHeight: eChartsConfig.legend.itemHeight,
+          top: eChartsConfig.legend.top,
+          right: eChartsConfig.legend.right,
+          textStyle: {
+            fontSize: eChartsConfig.legend.fontSize,
+            color: eChartsConfig.legend.color
+          }
+        },
+        tooltip: {
+          trigger: 'axis',
+            axisPointer: {
+            type: 'cross',
+              label: {
+              backgroundColor: '#6a7985'
+            }
+          }
+        },
+        xAxis: {
+          type: 'category',
+          data: res.chartLabels,
+          splitLine: eChartsConfig.xAxis.splitLine,
+          axisLabel: eChartsConfig.xAxis.axisLabel,
+          axisLine: eChartsConfig.xAxis.axisLine
+        },
+        yAxis: {
+          type: 'value',
+          splitLine: eChartsConfig.yAxis.splitLine,
+          axisLabel: eChartsConfig.yAxis.axisLabel,
+          axisLine: {
+            show: false
+          }
+        }
+      };
+
+      this.chart.series = [{
+        name: res.chartName,
+        data: res.chartValues,
+        type: 'line',
+        symbol: eChartsConfig.series.symbol,
+        symbolSize: eChartsConfig.series.symbolSize,
+        lineStyle: {...eChartsConfig.series.lineStyle, color: lineColors[2] },
+        itemStyle: { color: lineColors[2] }
+      }];
+    });
   }
 }
