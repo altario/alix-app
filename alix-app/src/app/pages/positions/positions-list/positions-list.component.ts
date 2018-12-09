1a90ab10c889595cc1487ad5dcc43b9d4f527c68
João Ribeiro <jonnybgod@gmail.com>
2018-12-09 19:03:26 +0000

    filter and sort lists based on arrays of ids

--- alix-app/src/app/pages/positions/positions-list/positions-list.component.ts
+++ alix-app/src/app/pages/positions/positions-list/positions-list.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { MockapiService } from '../../../services/mockapi.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const filterSortArray = []; // [2, 1, ...]
 
 @Component({
   selector: 'app-positions-list',
@@ -13,7 +16,16 @@ export class PositionsListComponent implements OnInit {
   public positions$: Observable<any>;
 
   constructor(private apiService: MockapiService) {
-    this.positions$ = this.apiService.getPositions();
+    this.positions$ = this.apiService.getPositions().pipe(
+      map(notifications => {
+        if (!filterSortArray.length) {
+          return notifications;
+        }
+        return notifications
+          .filter(a => filterSortArray.indexOf(a.id) !== -1)
+          .sort((a, b) => filterSortArray.indexOf(a.id) - filterSortArray.indexOf(b.id));
+      })
+    );
   }
 
   ngOnInit() {}
