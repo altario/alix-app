683d3e6e002a123b10d2dbab68e91e2635b443ff
Joseph Briggs <jbriggsr3@gmail.com>
2018-12-10 01:30:47 +0000

    Added data comparison in some graphs

---
+++ alix-app/src/app/data/data-comparison.ts
@@ -0,0 +1,22 @@
+export const dataComparison = {
+  priceEvolutionBySc: {
+    page: 'State of Conservation',
+    name: 'Price Evolution by State of Conservation',
+    values: [10150, 10150, 10150, 10150, 10150, 10150]
+  },
+  sqmPricePerStateOfConservationBarPlot: {
+    page: 'State of Conservation',
+    name: 'SQM Price per State of Conservation bar plot',
+    values: [1, 10150]
+  },
+  neighborhoodMktValueVsReplacementCost: {
+    page: 'Demand & Offer',
+    name: 'Market Value vs. Replacement Cost',
+    values: [10150, 10150, 10150, 10150, 10150, 10150, 10150, 10150, 10150, 10150, 10150]
+  },
+  priceSqmTodayVsOvertimeForSale: {
+    page: 'Demand & Offer',
+    name: 'Price Sqm Today vs Radius Average over Time',
+    values: [10150, 10150, 10150, 10150, 10150, 10150]
+  }
+}
