9be05fd6a4cc741d9d6af0e6082e6ae3f6fa2719
Rui Guedes <rui@Air-de-Rui.lan>
2019-01-17 22:07:24 +0000

    add 2 new blocks in performance page

--- alix-app/src/app/pages/positions/performance/performance.component.ts
+++ alix-app/src/app/pages/positions/performance/performance.component.ts
@@ -630,7 +630,7 @@ export class PerformanceComponent implements OnInit {
   weekdaypeaks = () => {
     const lineColors = ['#FFFFFF', '#00B5E9', '#7AC143'];
     const axisyLabel = JSON.parse(JSON.stringify(eChartsConfig.yAxis.axisLabel));
-    axisyLabel.formatter = function(value, index) {
+    axisyLabel.formatter = function (value, index) {
       const processedValue = numericalValues(value);
       return processedValue.round + ' ' + (processedValue.unitname ? processedValue.unitname + '€' : '');
     };
@@ -763,6 +763,286 @@ export class PerformanceComponent implements OnInit {
         itemStyle: { color: lineColors[2] },
         stack: 'one',
         areaStyle: {}
+      }
+    ];
+  }
+
+  ratingEvolution = () => {
+    const lineColors = ['#7AC143', '#00B5E9','#FFFFFF'];
+    const axisyLabel = JSON.parse(JSON.stringify(eChartsConfig.yAxis.axisLabel));
+    axisyLabel.formatter = function(value, index) {
+      const processedValue = numericalValues(value);
+      return processedValue.round + ' ' + (processedValue.unitname ? processedValue.unitname + '€' : '');
+    };
+
+    this.opts.ratingEvolution = {
+      title: {
+        text: 'RATING EVOLUTION', // #HC
+        top: '17px',
+        left: '16px',
+        textStyle: eChartsConfig.title
+      },
+      legend: {
+        data: [
+          {
+            name: 'God Save the Food',
+            icon: 'rect'
+          },
+          {
+            name: 'Competitors',
+            icon: 'rect'
+          }
+        ],
+        itemWidth: eChartsConfig.legend.itemWidth,
+        itemHeight: eChartsConfig.legend.itemHeight,
+        top: eChartsConfig.legend.top,
+        right: eChartsConfig.legend.right,
+        textStyle: {
+          fontSize: eChartsConfig.legend.fontSize,
+          color: eChartsConfig.legend.color
+        }
+      },
+      grid: eChartsConfig.grid,
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        }
+      },
+      xAxis: {
+        type: 'category',
+        data: [
+          'Jan 2018',
+          'Feb 2018',
+          'Mar 2018',
+          'Apr 2018',
+          'May 2018',
+          'Jun 2018',
+          'Jul 2018',
+          'Aug 2018',
+          'Sep 2018',
+          'Oct 2018',
+          'Nov 2018',
+          'Dec 2018',
+          'Jan 2019',
+        ],
+        splitLine: eChartsConfig.xAxis.splitLine,
+        axisLabel: eChartsConfig.xAxis.axisLabel,
+        axisLine: eChartsConfig.xAxis.axisLine
+      },
+      yAxis: {
+        // axisLabel: axisyLabel,
+        axisLabel: {
+          show: false
+        },
+        type: 'value',
+        splitLine: eChartsConfig.yAxis.splitLine,
+        axisLine: {
+          show: false
+        }
+      }
+    };
+
+    return [
+      {
+        name: 'God Save the Food',
+        data: [
+          3.8,
+          3.9,
+          3.9,
+          4.0,
+          4.0,
+          4.0,
+          4.0,
+          4.1,
+          4.1,
+          4.2,
+          4.2,
+          4.2,
+          4.3],
+        type: 'line',
+        symbol: eChartsConfig.series.symbol,
+        symbolSize: eChartsConfig.series.symbolSize,
+        lineStyle: { ...eChartsConfig.series.lineStyle, color: lineColors[0] },
+        itemStyle: { color: lineColors[0] }
+      },
+      {
+        name: 'Competitors',
+        data: [
+          4.2,
+          4.2,
+          4.2,
+          4.1,
+          4.0,
+          4.0,
+          4.0,
+          4.0,
+          4.0,
+          4.0,
+          4.0,
+          3.9,
+          3.9],
+        type: 'line',
+        symbol: eChartsConfig.series.symbol,
+        symbolSize: eChartsConfig.series.symbolSize,
+        lineStyle: { ...eChartsConfig.series.lineStyle, color: lineColors[1] },
+        itemStyle: { color: lineColors[1] }
+      }
+    ];
+  }
+
+  sentimentAnalysis = () => {
+    const lineColors = ['#00B5E9', '#7AC143', '#FFFFFF'];
+    const axisyLabel = JSON.parse(JSON.stringify(eChartsConfig.yAxis.axisLabel));
+    axisyLabel.formatter = function (value, index) {
+      const processedValue = numericalValues(value);
+      return processedValue.round + ' ' + (processedValue.unitname ? processedValue.unitname  : '');
+    };
+
+    this.opts.sentimentAnalysis = {
+      title: {
+        text: 'SENTIMENT ANALYSIS EVOLUTION', // #HC
+        top: '17px',
+        left: '16px',
+        textStyle: eChartsConfig.title
+      },
+      legend: {
+        enable: false,
+        data: [
+          {
+            name: 'Morning - 6.00 am > 12.00 PM',
+            icon: 'rect'
+          },
+          {
+            name: 'Afternoon - 12.01 am > 5.00 PM',
+            icon: 'rect'
+          },
+          {
+            name: 'Evening - 5.01 am > 6.30 PM',
+            icon: 'rect'
+          }
+        ],
+        itemWidth: eChartsConfig.legend.itemWidth,
+        itemHeight: eChartsConfig.legend.itemHeight,
+        top: eChartsConfig.legend.top,
+        right: eChartsConfig.legend.right,
+        textStyle: {
+          fontSize: eChartsConfig.legend.fontSize,
+          color: eChartsConfig.legend.color
+        }
+      },
+      grid: eChartsConfig.grid,
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        },
+        formatter: params => {
+          // tslint:disable-next-line:max-line-length
+          const colorSpan = color =>
+            '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' +
+            color +
+            '"></span>';
+          let rez = params[0].axisValue + '<br>';
+          params.forEach(item => {
+            const processedValue = numericalValues(item.data);
+            // tslint:disable-next-line:max-line-length
+            rez +=
+              colorSpan(item.color) +
+              ' ' +
+              item.seriesName +
+              ': ' +
+              processedValue.round +
+              ' ' +
+              (processedValue.unitname ? processedValue.unitname + '€' : '') +
+              '<br />';
+          });
+
+          return rez;
+        }
+      },
+      xAxis: {
+        type: 'category',
+        data: [
+          'Jan 2018',
+          'Feb 2018',
+          'Mar 2018',
+          'Apr 2018',
+          'May 2018',
+          'Jun 2018',
+          'Jul 2018',
+          'Aug 2018',
+          'Sep 2018',
+          'Oct 2018',
+          'Nov 2018',
+          'Dec 2018',
+          'Jan 2019',
+        ],
+        splitLine: eChartsConfig.xAxis.splitLine,
+        axisLabel: eChartsConfig.xAxis.axisLabel,
+        axisLine: eChartsConfig.xAxis.axisLine
+      },
+      yAxis: {
+        axisLabel: axisyLabel,
+        type: 'value',
+        splitLine: eChartsConfig.yAxis.splitLine,
+        axisLine: {
+          show: false
+        }
+      }
+    };
+
+    return [
+      {
+        name: 'Negative',
+        data: [2,
+          2,
+          4,
+          4,
+          6,
+          6,
+          6,
+          7,
+          7,
+          8,
+          8,
+          9,
+          10],
+        type: 'bar',
+        stack: 'one',
+        symbol: eChartsConfig.series.symbol,
+        symbolSize: eChartsConfig.series.symbolSize,
+        lineStyle: { ...eChartsConfig.series.lineStyle, color: lineColors[0] },
+        itemStyle: { color: lineColors[0] }
+      },
+      {
+        name: 'Positive',
+        data: [
+          42,
+          48,
+          50,
+          57,
+          63,
+          66,
+          73,
+          84,
+          92,
+          102,
+          116,
+          129,
+          140],
+        type: 'bar',
+        stack: 'one',
+        symbol: eChartsConfig.series.symbol,
+        symbolSize: eChartsConfig.series.symbolSize,
+        lineStyle: { ...eChartsConfig.series.lineStyle, color: lineColors[1] },
+        itemStyle: { color: lineColors[1] }
       }
     ];
   }
