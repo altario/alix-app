89b42638d7e6ee19b344efdc88824430d7abd449
Andre Pinto <a.pinix@gmail.com>
2019-09-30 11:35:46 +0100

    Updated files

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## Deploy
 
-Run `nps deploy.s3`
+Run in Terminal:
+
+- `brew install awscli` (you can install differently via [here](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html))
+- `nps deploy.s3`
---
+++ s3.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# AWS Config
+export AWS_ACCESS_KEY_ID="AKIAJN6KV56DSCESEE3Q"
+export AWS_SECRET_ACCESS_KEY="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"
+
+BUCKET="alix-cvc.altar.io"
+OUTPATH="s3website"
+
+aws s3 rm s3://$BUCKET --recursive
+aws s3 sync $OUTPATH s3://$BUCKET --region eu-west-1
