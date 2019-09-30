0678041c61ef1b36a3b4fe9ae05ed6e51315bb9a
Andre Pinto <a.pinix@gmail.com>
2019-09-30 12:00:03 +0100

    Update build

--- package-scripts.js
+++ package-scripts.js
@@ -4,7 +4,7 @@ module.exports = {
       s3: "nps build.s3 && AWS_ACCESS_KEY_ID=AKIAJN6KV56DSCESEE3Q AWS_SECRET_ACCESS_KEY=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 ./s3.sh",
     },
     build: {
-      s3: "cd alix-app && ng build --prod --output-path ../s3website"
+      s3: "cd alix-app && ng build --prod --base-href /alix-isp-credit-value-chain/ --output-path ../s3website"
     }
   }
 };
