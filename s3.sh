a83204552f60388d0031b70d36cb9a2d446b46c2
Andre Pinto <a.pinix@gmail.com>
2019-09-30 11:48:23 +0100

    Update build

--- s3.sh
+++ s3.sh
@@ -7,6 +7,10 @@ export AWS_SECRET_ACCESS_KEY="U9GkSfE1JrrpEQ79QqC6hlb4NHkYZjTf+ahbf7BR"
 
 BUCKET="alix-cvc.altar.io"
 OUTPATH="s3website"
+LOCALPATH="docs"
 
 aws s3 rm s3://$BUCKET --recursive
 aws s3 sync $OUTPATH s3://$BUCKET --region eu-west-1
+
+rm -rf "$LOCALPATH"/*
+cp -r "$OUTPATH"/* $LOCALPATH
